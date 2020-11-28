18dc820ba681524ec0323dfbd12943e42250342b
Sebastian <sebastianlarabarria@gmail.com>
2020-11-28 18:38:31 -0300

    Tutoriales generales

--- Actividades/Tutoriales_generales/Bot_Lara_1/botlara_test.py
+++ Actividades/Tutoriales_generales/Bot_Lara_1/botlara_test.py
@@ -1,17 +1,15 @@
-import slack
-import os
 from slack import WebClient
 from bot_lara import Bot_Lara
-
-
+import os
+import slack
 # Create a slack client
 slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN"))
 
-# Get a new coinbot
-bot_lara = Bot_Lara("#Alumno_SebastianLara")
+# Get a new NestorBot
+bot_lara = Bot_Lara("#playground")
 
 # Get the onboarding message payload
 message = bot_lara.get_message_payload()
 
-# Post the onboarding message in slack
+# Post the onboarding message in Slack
 slack_web_client.chat_postMessage(**message)
